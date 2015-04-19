1d3ed3bd50c48a107da60b488e693ceefd92f4d3
MicheleSr <mikefender@cryptolab.net>
2015-04-19 02:24:16 +0200

    schema cerberus integration

--- sbcatalog/default_settings.py
+++ sbcatalog/default_settings.py
@@ -216,31 +216,6 @@ schema = {
             }
         }
     },
-    'lastname': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 15,
-        'required': True,
-        # talk about hard constraints! For the purpose of the demo
-        # 'lastname' is an API entry-point, so we need it to be unique.
-        'unique': True,
-    },
-    # 'role' is a list, and can only contain values from 'allowed'.
-    'role': {
-        'type': 'list',
-        'allowed': ["author", "contributor", "copy"],
-    },
-    # An embedded 'strongly-typed' dictionary.
-    'location': {
-        'type': 'dict',
-        'schema': {
-            'address': {'type': 'string'},
-            'city': {'type': 'string'}
-        },
-    },
-    'born': {
-        'type': 'datetime',
-    },
 }
 
 supplier = {
